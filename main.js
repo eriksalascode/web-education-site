c5f365c80a882ea5731493ba1176ed34687c755e
Erik Salas <25646533+eriksalascode@users.noreply.github.com>
2022-05-16 10:02:40 -0500

    Completed the Courses Section

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//change navbar styles on scroll
+
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 0);
+});
