be6066f05dab87fc81f362921faee80235a7e6be
Erik Salas <25646533+eriksalascode@users.noreply.github.com>
2022-05-17 14:41:21 -0500

    Competed the FAQs Section

--- main.js
+++ main.js
@@ -7,3 +7,17 @@ window.addEventListener("scroll", () => {
     .querySelector("nav")
     .classList.toggle("window-scroll", window.scrollY > 0);
 });
+
+//show/hide faq answer
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+
+    //check icon
+    const icon = faq.querySelector(".faq__icon i");
+    icon.className =
+      icon.className === "uil uil-plus" ? "uil uil-minus" : "uil uil-plus";
+  });
+});
